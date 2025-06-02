e211df01d3802e58b6650c01138a0c3c43980c0f
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-06-02 12:33:48 +0200

    GR-18 create GETTER + SETTER

--- src/fr/humanbooster/fx/avis/business/Editeur.java
+++ src/fr/humanbooster/fx/avis/business/Editeur.java
@@ -6,4 +6,19 @@ public class Editeur {
 
     private String nom;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
