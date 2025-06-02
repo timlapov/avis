b338e81723b3c3e7dbc180d635563ce91a3c59cf
Timothée <83133135+timlapov@users.noreply.github.com>
2025-06-02 13:47:33 +0200

    Create Plateforme class

---
+++ src/fr/humanbooster/fx/avis/business/Plateforme.java
@@ -0,0 +1,23 @@
+package fr.humanbooster.fx.avis.business;
+
+public class Plateforme {
+    private Long id;
+    private String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
