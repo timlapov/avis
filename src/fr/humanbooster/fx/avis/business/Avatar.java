ac10c72782c4639e1ece0ff0dd26506d87238223
Timothée <83133135+timlapov@users.noreply.github.com>
2025-06-03 09:40:43 +0200

    Creation Avatar class

---
+++ src/fr/humanbooster/fx/avis/business/Avatar.java
@@ -0,0 +1,40 @@
+package fr.humanbooster.fx.avis.business;
+
+public class Avatar {
+    private Long id;
+    private String nom;
+    private static Long compteur = 0L;
+
+    public Avatar() {
+        this.id = compteur++;
+    }
+
+    public Avatar(String nom) {
+        this();
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
