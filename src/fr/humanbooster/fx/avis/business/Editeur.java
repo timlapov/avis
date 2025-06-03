9c86d2062434830a11d0bc8f4f3abaed9177e721
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-06-03 13:54:25 +0200

    Update Editeur.java

--- src/fr/humanbooster/fx/avis/business/Editeur.java
+++ src/fr/humanbooster/fx/avis/business/Editeur.java
@@ -1,18 +1,22 @@
 package fr.humanbooster.fx.avis.business;
 
+import java.util.Objects;
+
 public class Editeur {
 
     private Long id;
     private String nom;
+    private String Logo;
     private static Long compteur = 0L;
 
     public Editeur() {
         this.id = compteur++;
     }
 
-    public Editeur(String nom) {
+    public Editeur(String nom, String Logo) {
         this();
         this.nom = nom;
+        this.Logo = Logo;
     }
 
     public Long getId() {
@@ -31,8 +35,31 @@ public class Editeur {
         this.nom = nom;
     }
 
+    public String getLogo() {
+        return Logo;
+    }
+
+    public void setLogo(String logo) {
+        Logo = logo;
+    }
+
+    @Override
     public String toString() {
         return "Editeur " +
-                "[#id=" + id + ", nom= " + nom + "]";
+                "[#id=" + id +
+                ", nom= " + nom +
+                ", Logo=" + Logo + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Editeur editeur = (Editeur) o;
+        return Objects.equals(id, editeur.id) && Objects.equals(nom, editeur.nom) && Objects.equals(Logo, editeur.Logo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom, Logo);
     }
 }
